873b2f2872fe54c8dd7d1e9e6b2fe2e79fc85a08
tin270696 <lttin96@gmail.com>
2025-10-11 20:20:48 +0700

    reCAPTCHA + send mail

--- js/24880061.js
+++ js/24880061.js
@@ -1,4 +1,5 @@
 const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/users";
 
 const loadData = async (req, templateId, viewId) => {
     const response = await fetch(`${API}/${req}`);
@@ -9,4 +10,20 @@ const loadData = async (req, templateId, viewId) => {
     var context = { data: data };
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);
+}
+
+const getAuthenticateToken = async (username, password) => {
+    const response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({username, password})
+    });
+    const result = await response.json();
+    if(result.status == 200) {
+        return result.token;
+    }
+    throw new Error(result.message);
 }
